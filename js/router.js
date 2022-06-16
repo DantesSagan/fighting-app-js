555ede5591ac2446eee318743adbaa936ef4b09c
Alexsander Mikhaylov <mihaleksval@gmail.com>
2022-06-17 01:02:06 +0500

    changing width of about page

--- js/router.js
+++ js/router.js
@@ -1,68 +1,68 @@
-let routes = {};
-let templates = {};
+// let routes = {};
+// let templates = {};
 
-let app_div = document.getElementById('app');
+// let app_div = document.getElementById('app');
 
-function home() {
-  let div = document.createElement('div');
-  let link = document.createElement('a');
-  link.href = '#/game';
-  link.innerText = 'game';
+// function home() {
+//   let div = document.createElement('div');
+//   let link = document.createElement('a');
+//   link.href = '#/game';
+//   link.innerText = 'game';
 
-  div.innerHTML = '<h1>Home</h1>';
-  div.appendChild(link);
+//   div.innerHTML = '<h1>Home</h1>';
+//   div.appendChild(link);
 
-  app_div.appendChild(div);
-}
+//   app_div.appendChild(div);
+// }
 
-function about() {
-  let div = document.createElement('div');
-  let link = document.createElement('a');
-  link.href = '#/';
-  link.innerText = 'Home';
+// function about() {
+//   let div = document.createElement('div');
+//   let link = document.createElement('a');
+//   link.href = '#/';
+//   link.innerText = 'Home';
 
-  div.innerHTML = '<h1>About</h1>';
-  div.appendChild(link);
+//   div.innerHTML = '<h1>About</h1>';
+//   div.appendChild(link);
 
-  app_div.appendChild(div);
-}
+//   app_div.appendChild(div);
+// }
 
-function route(path, template) {
-  if (typeof template === 'function') {
-    return (routes[path] = template);
-  } else if (typeof template === 'string') {
-    return (routes[path] = templates[template]);
-  } else {
-    return;
-  }
-}
-function template(name, templateFunction) {
-  return (templates[name] = templateFunction);
-}
-template('home', function () {
-  home();
-});
+// function route(path, template) {
+//   if (typeof template === 'function') {
+//     return (routes[path] = template);
+//   } else if (typeof template === 'string') {
+//     return (routes[path] = templates[template]);
+//   } else {
+//     return;
+//   }
+// }
+// function template(name, templateFunction) {
+//   return (templates[name] = templateFunction);
+// }
+// template('home', function () {
+//   home();
+// });
 
-template('about', function () {
-  about();
-});
+// template('about', function () {
+//   about();
+// });
 
-route('/', 'home');
-route('/about', 'about');
+// route('/', 'home');
+// route('/about', 'about');
 
-function resolveRoute(route) {
-  try {
-    return routes[route];
-  } catch (e) {
-    throw new Error(`Route ${route} not found`);
-  }
-}
-function router(evt) {
-  let url = window.location.hash.slice(1) || '/';
-  let route = resolveRoute(url);
+// function resolveRoute(route) {
+//   try {
+//     return routes[route];
+//   } catch (e) {
+//     throw new Error(`Route ${route} not found`);
+//   }
+// }
+// function router(evt) {
+//   let url = window.location.hash.slice(1) || '/';
+//   let route = resolveRoute(url);
 
-  route();
-}
+//   route();
+// }
 
-window.addEventListener('load', router);
-window.addEventListener('hashchange', router);
+// window.addEventListener('load', router);
+// window.addEventListener('hashchange', router);
